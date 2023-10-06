11b3f60279e3a9e153a70d9fa0b734631e5c8130
Vish Hulikal <vhulikal@yahoo.com>
2023-10-06 16:38:32 +0000

    Version 3

--- cluster.py
+++ cluster.py
@@ -1,12 +1,12 @@
-#! /usr/bin/env python3.7
+#! /usr/local/bin/python3.11
 from netapp_ontap import config
 from netapp_ontap import HostConnection
 from netapp_ontap.resources import Cluster
 import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-conn = HostConnection("192.168.0.111", username = "admin", password = "Netapp1!", verify = False)
-config.CONNECTION = conn  
+conn = HostConnection("192.168.0.101", username = "admin", password = "Netapp1!", verify = False)
+config.CONNECTION = conn
 clus = Cluster()
 clus.get()
 print (clus.version)
